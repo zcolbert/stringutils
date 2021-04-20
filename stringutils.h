3ff65d4c67f251baeeaadef6690c0b902eea2a9a
Zac Colbert <zcolbert1993@gmail.com>
2021-04-20 15:20:51 -0700

    Remove whitespace keyword from strip methods

--- stringutils.h
+++ stringutils.h
@@ -15,14 +15,14 @@ std::vector<std::string> split(const std::string& orig);
 std::vector<std::string> split(const std::string& orig, char delim);
 std::vector<std::string> split(const std::string& orig, const std::string& delim);
 
+std::string lstrip(const std::string& orig);
 std::string lstrip(const std::string& orig, char delim);
 std::string lstrip(const std::string& orig, const std::string& delim);
-std::string lstrip_whitespace(const std::string& orig);
 
+std::string rstrip(const std::string& orig);
 std::string rstrip(const std::string& orig, char delim);
 std::string rstrip(const std::string& orig, const std::string& delim);
-std::string rstrip_whitespace(const std::string& orig);
 
+std::string strip(const std::string& orig);
 std::string strip(const std::string& orig, char delim);
 std::string strip(const std::string& orig, const std::string& delim);
-std::string strip_whitespace(const std::string& orig);
