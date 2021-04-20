53612454105e31dec2f65741c5e32a94114f8b9d
Zac Colbert <zcolbert1993@gmail.com>
2021-04-20 15:48:56 -0700

    Implement lstrip with char parameter

--- stringutils.cpp
+++ stringutils.cpp
@@ -1,12 +1,13 @@
 #include "stringutils.h"
 #include <sstream>
 #include <stdexcept>
+#include <iostream>
 
 
 std::string join(const std::vector<std::string>& parts)
 {
     if (parts.empty()) { return ""; }
-    if (parts.size() == 1) { return parts.at(0); }
+    if (parts.size() == 1) { return parts[0]; }
 
     size_t size = 0;
     for (const auto& s: parts) { size += s.size(); }
@@ -18,7 +19,7 @@ std::string join(const std::vector<std::string>& parts)
 std::string join(const std::vector<std::string>& parts, char delim)
 {
     if (parts.empty()) { return ""; }
-    if (parts.size() == 1) { return parts.at(0); }
+    if (parts.size() == 1) { return parts[0]; }
 
     size_t size = parts.size() - 1;
     for (const auto& s: parts) { size += s.size(); }
@@ -26,16 +27,16 @@ std::string join(const std::vector<std::string>& parts, char delim)
     result.reserve(size);
 
     for (size_t i=0; i<parts.size()-1; ++i) {
-        result.append(parts.at(i));
+        result.append(parts[i]);
         result += delim;
     }
-    result.append(parts.at(parts.size()-1));
+    result.append(parts[parts.size()-1]);
     return result;
 }
 std::string join(const std::vector<std::string>& parts, const std::string& delim)
 {
     if (parts.empty()) { return ""; }
-    if (parts.size() == 1) { return parts.at(0); }
+    if (parts.size() == 1) { return parts[0]; }
 
     size_t size = delim.size() * (parts.size()-1);
     for (const auto& s: parts) { size += s.size(); }
@@ -43,16 +44,20 @@ std::string join(const std::vector<std::string>& parts, const std::string& delim
     result.reserve(size);
 
     for (size_t i=0; i<parts.size()-1; ++i) {
-        result.append(parts.at(i));
+        result.append(parts[i]);
         result += delim;
     }
-    result.append(parts.at(parts.size()-1));
+    result.append(parts[parts.size()-1]);
     return result;
 }
 
 std::string replace(const std::string& orig, char from, char to)
 { throw std::runtime_error("Not implemented"); }
+
 std::string replace(const std::string& orig, const std::string& from, const std::string& to)
+{ throw std::runtime_error("Not implemented"); }
+
+std::vector<std::string> split(const std::string& orig)
 { throw std::runtime_error("Not implemented"); }
 
 std::vector<std::string> split(const std::string& orig, char delim)
@@ -82,23 +87,39 @@ std::vector<std::string> split(const std::string& orig, const std::string& delim
     return parts;
 }
 
-std::string l_strip(const std::string& orig, char delim)
-{ throw std::runtime_error("Not implemented"); }
-std::string l_strip(const std::string& orig, const std::string& delim)
-{ throw std::runtime_error("Not implemented"); }
-std::string l_strip_whitespace(const std::string& orig)
+std::string lstrip(const std::string& orig)
 { throw std::runtime_error("Not implemented"); }
 
-std::string r_strip(const std::string& orig, char delim)
-{ throw std::runtime_error("Not implemented"); }
-std::string r_strip(const std::string& orig, const std::string& delim)
-{ throw std::runtime_error("Not implemented"); }
-std::string r_strip_whitespace(const std::string& orig)
+std::string lstrip(const std::string& orig, char delim)
+{
+    size_t pos = orig.find_first_not_of(delim);
+    if (pos == 0) {
+        return orig;
+    } else if (pos == std::string::npos) {
+        return "";
+    } else {
+        return orig.substr(pos, orig.length()-pos);
+    }
+}
+
+std::string lstrip(const std::string& orig, const std::string& delim)
 { throw std::runtime_error("Not implemented"); }
 
-std::string strip(const std::string& orig, char delim)
+std::string rstrip(const std::string& orig)
 { throw std::runtime_error("Not implemented"); }
-std::string strip(const std::string& orig, const std::string& delim)
+
+std::string rstrip(const std::string& orig, char delim)
 { throw std::runtime_error("Not implemented"); }
-std::string strip_whitespace(const std::string& orig)
+
+std::string rstrip(const std::string& orig, const std::string& delim)
 { throw std::runtime_error("Not implemented"); }
+
+std::string strip(const std::string& orig) {
+    return rstrip(lstrip(orig));
+}
+std::string strip(const std::string& orig, char delim) {
+    return rstrip(lstrip(orig, delim), delim);
+}
+std::string strip(const std::string& orig, const std::string& delim) {
+    return rstrip(lstrip(orig, delim), delim);
+}
